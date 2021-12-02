e38d906bce760202254c0dad3f5295a8b993a162
Simon Cuany <47849679+SimonCuany@users.noreply.github.com>
2021-12-02 11:08:13 +0100

    add Creation ticket code

---
+++ Création tickets/Création tickets/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿
+namespace Création_tickets
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbName = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.cmbType = new System.Windows.Forms.ComboBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtxbDesc = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCaractere = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txbName
+            // 
+            this.txbName.Location = new System.Drawing.Point(34, 161);
+            this.txbName.Name = "txbName";
+            this.txbName.Size = new System.Drawing.Size(100, 23);
+            this.txbName.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 23F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTitle.Location = new System.Drawing.Point(88, 28);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(292, 42);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Création de tickets";
+            // 
+            // cmbType
+            // 
+            this.cmbType.FormattingEnabled = true;
+            this.cmbType.Location = new System.Drawing.Point(442, 140);
+            this.cmbType.Name = "cmbType";
+            this.cmbType.Size = new System.Drawing.Size(121, 23);
+            this.cmbType.TabIndex = 2;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(34, 143);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(102, 15);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "Nom de l\'incident";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(335, 140);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Type de problème";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(585, 374);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 9;
+            this.btnClose.Text = "Quitter";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(34, 432);
+            this.monthCalendar1.MinDate = new System.DateTime(2000, 1, 1, 0, 0, 0, 0);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.ShowToday = false;
+            this.monthCalendar1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 419);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 15);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Date de la panne";
+            // 
+            // rtxbDesc
+            // 
+            this.rtxbDesc.Location = new System.Drawing.Point(34, 234);
+            this.rtxbDesc.MaxLength = 300;
+            this.rtxbDesc.Name = "rtxbDesc";
+            this.rtxbDesc.Size = new System.Drawing.Size(222, 131);
+            this.rtxbDesc.TabIndex = 12;
+            this.rtxbDesc.Text = "";
+            this.rtxbDesc.TextChanged += new System.EventHandler(this.rtxbDesc_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 15);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Description du problème";
+            // 
+            // lblCaractere
+            // 
+            this.lblCaractere.AutoSize = true;
+            this.lblCaractere.Location = new System.Drawing.Point(32, 369);
+            this.lblCaractere.Name = "lblCaractere";
+            this.lblCaractere.Size = new System.Drawing.Size(13, 15);
+            this.lblCaractere.TabIndex = 14;
+            this.lblCaractere.Text = "0";
+            this.lblCaractere.Click += new System.EventHandler(this.lblCaractere_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(84, 368);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 15);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "caractères max";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 369);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 15);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "/ 300";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(442, 374);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Envoyer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(277, 419);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 15);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Heure de la panne";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 646);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblCaractere);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rtxbDesc);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.cmbType);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.txbName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbName;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.ComboBox cmbType;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtxbDesc;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCaractere;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Création tickets/Création tickets/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Création_tickets
+{
+
+   
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void rtxbDesc_TextChanged(object sender, EventArgs e)
+        {
+            string txtcar = rtxbDesc.Text;
+            int length = txtcar.Length;
+
+            lblCaractere.Text = length.ToString();
+        }
+
+        private void lblCaractere_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}

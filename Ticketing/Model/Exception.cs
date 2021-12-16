581a218b91c131ed9951548cf56cd6d3cfc6fb8e
Yann Fanha <yannfnha@gmail.com>
2021-12-16 15:37:29 +0100

    Revert "Revert "Création fonction de connexion + nouvel exception pour la connexion (Mauvais mot de passe)""
    This reverts commit 542fde2ae14bf4a8dc43db3991151696b2001c73.

---
+++ Ticketing/Model/Exception.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticketing
+{
+    public class BadPasswordException : Exception
+    {
+        public BadPasswordException() : base ("Mot de passe incorrecte")
+        {
+        }
+    }
+}

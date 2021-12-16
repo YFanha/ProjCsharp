323d9071bc6c9849eda2d805de3d4c610679d04e
Yann Fanha <yannfnha@gmail.com>
2021-12-16 09:34:00 +0100

    Contructeur pour ticket déjà créer (récupérer dans la base de données)

--- Ticketing/Model/Ticket.cs
+++ Ticketing/Model/Ticket.cs
@@ -28,6 +28,20 @@ namespace Ticketing
         const int DEFAULT_STATES = 1;
         const int DEFAULT_CATEGORY = 5;
 
+        //Nom des colonnes
+        const string TITLE = "title";
+        const string DESCRIPTION = "description";
+        const string CATEGORY = "category_id";
+        const string STATE = "state_id";
+        const string MANAGER = "manager_id";
+        const string OPENING_DATE = "openingDate";
+        const string OPENING_PERSON = "openingPerson_id";
+        const string CLOSING_DATE = "closingDate";
+        const string CLOSING_PERSON = "closingPerson_id";
+        const string LAST_MODIFIED_DATE = "lastModifiedDate";
+        const string LAST_MODIFIED_PERSON = "lastModifiedPerson_id";
+
+
 
         // Constructeur pour nouveau ticket
         public Ticket(string title, string description, int openingPersonId)
@@ -45,30 +59,65 @@ namespace Ticketing
         }
 
         //Constructeur pour ticket déjà enregistré dans la base de données
-        private Ticket(int id, string title)
-        { 
+        private Ticket(int id, string title, string description, int category_id, int state_id, int manager_id, DateTime opening, int openingPerson, DateTime closing, int closingPerson, DateTime lastModified, int lastModifiedPerson)
+        {
+            _title = title;
+            _description = description;
+            _categoryId = category_id;
+            _statesId = state_id;
+            _managerId = Technician.RandomTechnician(); //
+            _openingDate = opening;
+            _openeningPersonId = openingPerson;
+            _closingDate = closing;
+            _closingPersonId = closingPerson;
+            _lastModifiedDate = lastModified;
+            _lastModifiedPersonId = lastModifiedPerson;
         }
 
-        //Trouver les données d'un ticket grace à son
+        //Trouver les données d'un ticket grace à son id
         public static Ticket Find(int id)
         {
-            string sql = "SELECT * FROM ticket WHERE id = " + id + ";";
-            MySqlCommand sqlQuery = new MySqlCommand(sql, DatabaseInteractions.GetConnection());
-            MySqlDataReader reader = sqlQuery.ExecuteReader();
+            MySqlDataReader reader = new MySqlCommand("SELECT * FROM ticket WHERE id = " + id + ";", DatabaseInteractions.GetConnection()).ExecuteReader();
+
+            string title = null;
+            string description = null;
+
+            int category_id = 0;
+            int state_id = 0;
+            int manager_id = 0;
+            DateTime opening = DateTime.Now;
+            int openingPerson = 0;
+            DateTime closing = DateTime.Now;
+            int closingPerson = 0;
+            DateTime lastmodified = DateTime.Now;
+            int lastmodifiedPerson = 0;
 
             while (reader.Read())
             {
-                
+                title = reader.GetString(TITLE);
+                description = reader.GetString(DESCRIPTION);
+                category_id = reader.GetInt32(CATEGORY);
+                state_id = reader.GetInt32(STATE);
+                manager_id = reader.GetInt32(MANAGER);
+                opening = reader.GetDateTime(OPENING_DATE);
+                openingPerson = reader.GetInt32(OPENING_PERSON);
+                closing = reader.GetDateTime(CLOSING_DATE);
+                closingPerson = reader.GetInt32(CLOSING_PERSON);
+                lastmodified = reader.GetDateTime(LAST_MODIFIED_DATE);
+                lastmodifiedPerson = reader.GetInt32(LAST_MODIFIED_PERSON);
             }
 
-
-            throw new NotImplementedException();
+            return new Ticket(id, title, description, category_id, state_id, manager_id, opening, openingPerson, closing, closingPerson, lastmodified, lastmodifiedPerson);
         }
 
         //Enregistrer le ticket dans la base de données
         public void Save()
         {
-            throw new NotImplementedException();
+            MySqlCommand sqlQuery = new MySqlCommand("", _connection);
+            try
+            {
+                
+            }
         }
     }
 }
